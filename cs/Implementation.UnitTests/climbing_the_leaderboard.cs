d85b4c39ca27345ad36a99de8387edf2678e8088
Ruskin Dantra <ruskin.dantra@gmail.com>
2018-12-24 00:01:25 +1300

    More test cases

--- cs/Implementation.UnitTests/climbing_the_leaderboard.cs
+++ cs/Implementation.UnitTests/climbing_the_leaderboard.cs
@@ -20,7 +20,7 @@ namespace Implementation.UnitTests
         [Theory]
         [InlineData("7", "4", "100 100 50 40 40 20 10", "5 25 50 120", new[] {"6", "4", "2", "1"})]
         [InlineData("6", "5", "100 90 90 80 75 60", "50 65 77 90 102", new[] {"6", "5", "4", "2", "1"})]
-        public void sample_test_case_1(string m, string n, string scores, string alice, string[] expected)
+        public void sample_test_cases(string m, string n, string scores, string alice, string[] expected)
         {
             var fixture = new Fixture();
             fixture.Customize(new AutoFixture.AutoMoq.AutoMoqCustomization());
@@ -43,8 +43,34 @@ namespace Implementation.UnitTests
             var sut = fixture.Create<ClimbingTheLeaderboard>();
             sut.Solve();
             
-            actualOutput.Should().BeEquivalentTo(expected).And.BeInDescendingOrder();
+            actualOutput.Should().BeEquivalentTo(expected).And.BeInDescendingOrder();            
+        }
+        
+        [Fact]
+        public void custom_1()
+        {
+            var fixture = new Fixture();
+            fixture.Customize(new AutoFixture.AutoMoq.AutoMoqCustomization());
+
+            var actualOutput = new List<string>();
+
+            var inputReaderMock = _mockRepository.Create<IInputReader>();
+            var outputWriterMock = _mockRepository.Create<IOutputWriter>();
+
+            inputReaderMock.SetupSequence(i => i.ReadLine())
+                .Returns("10")
+                .Returns("200 200 185 180 160 150 150 150 150 140")
+                .Returns("5")
+                .Returns("140 150 160 170 180");
+            outputWriterMock.Setup(o => o.WriteLine(It.IsAny<string>())).Callback<string>(s => actualOutput.Add(s));
             
+            fixture.Inject(inputReaderMock);
+            fixture.Inject(outputWriterMock);
+            
+            var sut = fixture.Create<ClimbingTheLeaderboard>();
+            sut.Solve();
+            
+            actualOutput.Should().BeEquivalentTo(new[] {"6", "5", "4", "4", "3"}).And.BeInDescendingOrder();            
         }
     }
 }
